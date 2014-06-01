6b27d5a64e7feecb1b0dddc43f7bc5b24cd3fd6c
Hedley Roos <hedley@boo>
2014-06-01 22:11:09 +0200

    New rules

--- script.rss.explorer/default.py
+++ script.rss.explorer/default.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009-2013 Hedley Roos (hedleyroos@gmail.com)
+# Copyright (C) 2014- Hedley Roos (hedleyroos@gmail.com)
 #
 # This program is free software; you can redistribute it and/or modify it under the terms 
 # of the GNU General Public License as published by the Free Software Foundation; 
--- service.rss.explorer/default.py
+++ service.rss.explorer/default.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#     Copyright (C) 2013 Team-XBMC
+#     Copyright (C) 2014 Hedley Roos (hedleyroos@gmail.com)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
